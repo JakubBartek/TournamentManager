32609977588ab00ae207fdd70b9e61c979d71cab
Jakub Bartek <xbartek2@fi.muni.cz>
2025-07-02 19:33:14 +0200

    changing routing urls

--- TournamentManager/frontend/src/App.tsx
+++ TournamentManager/frontend/src/App.tsx
@@ -1,42 +1,51 @@
-import Home from "./pages/Home";
-import Schedule from "./pages/Schedule";
-import Map from "./pages/Map";
-import Teams from "./pages/Teams";
-import { BaseLayout } from "./pages/BaseLayout";
-import "./App.css"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from './pages/Home'
+import Schedule from './pages/Schedule'
+import Map from './pages/Map'
+import Teams from './pages/Teams'
+import { BaseLayout } from './pages/BaseLayout'
+import './App.css'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import SelectTournament from './pages/SelectTournament'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/select',
+    Component: SelectTournament,
+  },
+  {
+    path: '/:tournamentId',
     Component: BaseLayout,
     children: [
       {
-        path: "/",
+        path: 'home', // toto je cesta /:tournamentId
         Component: Home,
       },
       {
-        path: "/schedule",
+        path: 'schedule',
         Component: Schedule,
       },
       {
-        path: "/map",
+        path: 'map',
         Component: Map,
       },
       {
-        path: "/teams",
+        path: 'teams',
         Component: Teams,
       },
     ],
   },
   {
-    path: "*",
-    element: <div>404 Not Found</div>, // TODO: ERROR PAGE
-  }
-]);
+    path: '/',
+    element: <Navigate to='/select' />,
+  },
+  {
+    path: '*',
+    element: <div>404 Not Found</div>,
+  },
+])
 
 function App() {
   return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
