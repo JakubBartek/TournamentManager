03d80d6cec07dbbd873e86e7637097fc932103c7
Jakub Bartek <xbartek2@fi.muni.cz>
2025-07-02 19:35:33 +0200

    feat: updating home page

--- TournamentManager/frontend/src/pages/Home.tsx
+++ TournamentManager/frontend/src/pages/Home.tsx
@@ -1,42 +1,28 @@
 import { Card, CardContent } from '@/components/ui/card'
-import type { Game } from '../types/game.ts' // Assuming Game is exported from a shared file
+import type { Game } from '../types/game.ts'
 import { format } from 'date-fns'
 import { useGames } from '@/hooks/useGame'
-import { useQueryClient } from '@tanstack/react-query'
-import { useSearchParams } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Home() {
-  const [searchParams] = useSearchParams()
-  const queryClient = useQueryClient()
   const now = new Date()
   const GAME_DURATION_MINUTES = 60
+  const { tournamentId } = useParams<{ tournamentId: string }>()
+  const { data: games, isLoading, error } = useGames(tournamentId ?? '')
+
+  if (!tournamentId) {
+    return <div>Error: Tournament ID is missing</div>
+  }
 
   let currentGame: Game | null = null
   let nextGame: Game | null = null
 
-  useEffect(() => {
-    queryClient.invalidateQueries({
-      queryKey: ['gamesHome'],
-    })
-  }, [searchParams, queryClient])
-
-  const {
-    data: games,
-    isLoading,
-    error,
-  } = useGames('6ac7f8f0-7e75-4456-80ef-ebc342f1ceeb')
-
-  if (isLoading) return <div>Načítavam hry...</div>
-  if (error) return <div>Chyba: {(error as Error).message}</div>
+  if (isLoading) return <div>Loading games...</div>
+  if (error) return <div>Error: {(error as Error).message}</div>
 
   if (games === undefined) {
     return <div>Loading...</div>
   }
-
-  console.log('games =', games)
-  console.log('typeof games =', typeof games)
-  console.log('isArray =', Array.isArray(games))
 
   for (let i = 0; i < games.length; i++) {
     const game = games[i]
@@ -53,17 +39,16 @@ export default function Home() {
       nextGame = game
       break
     }
+
+    // TODO: REMOVE--- THIS IS ONLY TO SHOW SOMETHING IN THE UI
+    currentGame = game
+    nextGame = games[i + 1] ?? null
+    break
   }
 
   return (
     <div className='flex flex-col gap-2'>
-      <ul>
-        {games?.map((game) => (
-          <li key={game.id}>
-            {game.team1} vs {game.team2}
-          </li>
-        ))}
-      </ul>
+      {}
       {currentGame && (
         <Card className='w-full'>
           <CardContent className=''>
