c216682f6c6b772ba5b2d071b2009603b3653d51
Jakub Bartek <jakub.bartek145@gmail.com>
2025-08-09 01:26:20 +0200

    important todo

--- TournamentManager/backend/src/auth/auth.router.ts
+++ TournamentManager/backend/src/auth/auth.router.ts
@@ -1,12 +1,13 @@
-import { Router } from "express"
-import { getProfile, login, refreshToken, register } from "./auth.controller"
-import { isAuthenticated } from "../middlewares"
+import { Router } from 'express'
+import { getProfile, login, refreshToken, register } from './auth.controller'
+import { isAuthenticated } from '../middlewares'
 
 const router = Router()
 
-router.post("/register", register)
-router.post("/login", login)
-router.post("/refreshToken", refreshToken)
-router.get("/profile", isAuthenticated, getProfile)
+// TODO: VERIFY THAT IT IS COMING FROM TOURNAMENT ADMIN
+router.post('/register', register)
+router.post('/login', login)
+router.post('/refreshToken', refreshToken)
+router.get('/profile', isAuthenticated, getProfile)
 
 export default router
